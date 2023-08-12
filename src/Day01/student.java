c7f53d26ba3920c9ed66e1f8017c4f6cbf14d3b2
DavitiChkhetiani <davitichkhetiani@gmail.com>
2023-08-12 17:04:15 -0400

    first commit

---
+++ src/Day01/student.java
@@ -0,0 +1,10 @@
+package Day01;
+
+import java.sql.SQLOutput;
+
+public class student {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int a = 123;
+    }
+}
